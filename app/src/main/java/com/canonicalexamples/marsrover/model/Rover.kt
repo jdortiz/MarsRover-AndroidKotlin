1f5ec95b8f5a5762e77091e9d710044cf14d4961
Jorge D. Ortiz Fuentes <jdortiz@gmail.com>
2018-03-14 19:43:02 +0100

    Implement rotation to the right ("R") command.

---
+++ app/src/main/java/com/canonicalexamples/marsrover/model/Rover.kt
@@ -0,0 +1,36 @@
+package com.canonicalexamples.marsrover.model
+
+/**
+ * 20180314. Initial version created by jorge
+ * for a Canonical Examples training.
+ *
+ * Copyright 2018 Jorge D. Ortiz Fuentes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class Rover(var x: Int, var y: Int, var direction: Char) {
+    fun execute(commands: String) {
+        for (command in commands) {
+            if (command == 'R') {
+                direction = when (direction) {
+                    'N' -> 'E'
+                    'E' -> 'S'
+                    'S' -> 'W'
+                    else -> {
+                        'N'
+                    }
+                }
+            }
+        }
+    }
+}
